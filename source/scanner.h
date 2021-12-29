df98a54c2af3bbcd7a4c66ca9c10409b9bb084dc
ahmed genina <76185731+ahmedgenina97@users.noreply.github.com>
2021-12-29 03:18:07 +0200

    parser prototype

--- source/scanner.h
+++ source/scanner.h
@@ -11,7 +11,7 @@ std::string RES_WORDS[] = { "if","then","else","end","repeat","until","read","wr
 
 
 struct tokens {
-    std::string token,token_type;
+	std::string token, token_type;
 };
 
 bool isDigit(char d) { return (d >= '0' && d <= '9'); }
@@ -20,132 +20,132 @@ bool isLetter(char l) { return (l >= 'a' && l <= 'z' || l >= 'A' && l <= 'Z'); }
 
 bool isSpace(char s) { return (s == ' ' || s == '\t' || s == '\n'); }
 
-bool isSymbol(char c) { return (c == '+' || '-' || '*' || '/' || '=' || '<' ||'>'|| '(' || ')' || ';'); }
+bool isSymbol(char c) { return (c == '+' || '-' || '*' || '/' || '=' || '<' || '>' || '(' || ')' || ';'); }
 
-void getToken(std::string l,std::list<tokens>&code_tokens)
+void getToken(std::string l, std::list<tokens>& code_tokens)
 {
-    std::string mytoken;
-    bool res_flag = false;
-    int i = 0;
-    tokens token;
-    scan_state state = START;
+	std::string mytoken;
+	bool res_flag = false;
+	int i = 0;
+	tokens token;
+	scan_state state = START;
 
-    while (state != DONE) {
+	while (state != DONE) {
 
-        switch (state) {
-        case START:
-            if (isSpace(l[i])) {
-                i++;
-                if (i == l.length()) state = DONE;
-                else state = START;
-            }
-            else if (isDigit(l[i])) {
-                state = INNUM;
-            }
-            else if (isLetter(l[i])) {
-                state = INID;
-            }
-            else if (l[i] == ':') {
-                state = INASSIGN;
-            }
-            else if (l[i] == '{') {
-                i++;
-                state = INCOMMENT;
-            }
-            else if (isSymbol(l[i])) {
-                switch (l[i]) {
-                case ';': token = { std::string(1,(l[i])),"SEMICOLON" }; code_tokens.emplace_back(token); break;
-                case '+': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '-': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '*': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '/': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '=': token = { std::string(1,(l[i])),"EQUAL_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '<': token = { std::string(1,(l[i])),"LESS_OPERATION" }; code_tokens.emplace_back(token); break;
-                case '>': token = { std::string(1,(l[i])),"GREATER_OPERATION" }; code_tokens.emplace_back(token); break;
+		switch (state) {
+		case START:
+			if (isSpace(l[i])) {
+				i++;
+				if (i == l.length()) state = DONE;
+				else state = START;
+			}
+			else if (isDigit(l[i])) {
+				state = INNUM;
+			}
+			else if (isLetter(l[i])) {
+				state = INID;
+			}
+			else if (l[i] == ':') {
+				state = INASSIGN;
+			}
+			else if (l[i] == '{') {
+				i++;
+				state = INCOMMENT;
+			}
+			else if (isSymbol(l[i])) {
+				switch (l[i]) {
+				case ';': token = { std::string(1,(l[i])),"SEMICOLON" }; code_tokens.emplace_back(token); break;
+				case '+': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '-': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '*': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '/': token = { std::string(1,(l[i])),"MATH_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '=': token = { std::string(1,(l[i])),"EQUAL_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '<': token = { std::string(1,(l[i])),"LESS_OPERATION" }; code_tokens.emplace_back(token); break;
+				case '>': token = { std::string(1,(l[i])),"GREATER_OPERATION" }; code_tokens.emplace_back(token); break;
 
-                }
-                i++;
-                if (i == l.length()) state = DONE;
-                else state = START;
-            }
-            else state = DONE;
-            break;
+				}
+				i++;
+				if (i == l.length()) state = DONE;
+				else state = START;
+			}
+			else state = DONE;
+			break;
 
-        case INCOMMENT:
-            if (state == INCOMMENT) {
-                while (l[i] != '}') {
-                    i++;
-                }
-                i++;
-                if (i == l.length()) state = DONE;
-                else state = START;
-            }
-            break;
-            
-        case INNUM:
-            while (isDigit(l[i])||l[i]=='.') {
-                mytoken += l[i];
-                i++;
-            }
+		case INCOMMENT:
+			if (state == INCOMMENT) {
+				while (l[i] != '}') {
+					i++;
+				}
+				i++;
+				if (i == l.length()) state = DONE;
+				else state = START;
+			}
+			break;
 
-            token = { mytoken,"NUMBER" };
-            code_tokens.emplace_back(token);
+		case INNUM:
+			while (isDigit(l[i]) || l[i] == '.') {
+				mytoken += l[i];
+				i++;
+			}
+
+			token = { mytoken,"NUMBER" };
+			code_tokens.emplace_back(token);
 
 
-            mytoken = "";
-            if (i == l.length()) state = DONE;
-            else state = START;
-            break;
+			mytoken = "";
+			if (i == l.length()) state = DONE;
+			else state = START;
+			break;
 
-        case INID:
+		case INID:
 
-            while (isLetter(l[i])) {
-                mytoken += l[i];
-                i++;
-            }
+			while (isLetter(l[i])) {
+				mytoken += l[i];
+				i++;
+			}
 
-            for (int i = 0; i < 8; i++)
-            {
-                if (RES_WORDS[i] == mytoken) res_flag = 1;
-            }
+			for (int i = 0; i < 8; i++)
+			{
+				if (RES_WORDS[i] == mytoken) res_flag = 1;
+			}
 
-            if (res_flag)
-            {
-                token = { mytoken,"RESERVED_WORD" };
-                code_tokens.emplace_back(token);
+			if (res_flag)
+			{
+				token = { mytoken,"RESERVED_WORD" };
+				code_tokens.emplace_back(token);
 
-            }
-        
-                else
+			}
 
-                {
-                    token={mytoken,"IDENTIFEIER"};
-                    code_tokens.emplace_back(token);
+			else
 
-                }
+			{
+				token = { mytoken,"IDENTIFEIER" };
+				code_tokens.emplace_back(token);
 
-                mytoken = "";
-                res_flag = 0;
-                if (i == l.length()) state = DONE;
-                else state = START;
-                break;
+			}
 
-            case INASSIGN:
-                if (l[i] == ':') {
-                    i += 2;
-                    token={":=","ASSIGNMENT"};
-                    code_tokens.emplace_back(token);
-                    state = START;
-                }
-                else {
-                    if (i == l.length()) state = DONE;
-                    else state = START;
-                }
-                break;
-            case DONE:
-                break;
-        }
-    }
+			mytoken = "";
+			res_flag = 0;
+			if (i == l.length()) state = DONE;
+			else state = START;
+			break;
+
+		case INASSIGN:
+			if (l[i] == ':') {
+				i += 2;
+				token = { ":=","ASSIGNMENT" };
+				code_tokens.emplace_back(token);
+				state = START;
+			}
+			else {
+				if (i == l.length()) state = DONE;
+				else state = START;
+			}
+			break;
+		case DONE:
+			break;
+		}
+	}
 }
 
 
